8b0d9e977dc86a28efe10d9f42c4dcf30a3b31c3
pranjal <pranjalkansal@gmail.com>
2016-04-26 13:31:18 +0530

    initial git

---
+++ README.md
@@ -0,0 +1,1 @@
+This project is created on node using express framework
---
+++ server.js
@@ -0,0 +1,21 @@
+/*******************************************************************************
+*                                                                              *
+*         File: server.js                                                      *
+*         Functionality: Entery point of node application                      *
+*                                                                              *
+*******************************************************************************/
+
+// Enable strict mode
+'use strict';
+
+// Get all dependencies in index file
+
+// Get vendors
+const server = require('express')();
+// End vendors
+
+// Get custom files
+const Config = require('Constants/index.js');
+// End custom files import
+
+// End of dependencies import
