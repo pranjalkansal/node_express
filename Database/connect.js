a42be5da2ad92db9fc71b67bf3b567e75497ba26
pranjal <pranjalkansal@gmail.com>
2016-05-30 23:48:08 +0530

    token creation added in parallel

--- Database/connect.js
+++ Database/connect.js
@@ -13,9 +13,29 @@ const mysql = require('mysql');
 
 // Get custom files.
 const db_config = require('../Constants/index.js').BASIC;
+const db_query = require('../Constants/index.js').QUERY;
 
 // Initiate mysql connection.
 var mysql_connection = mysql.createConnection(db_config.database);
+
+// Create connection to mySQL database.
+mysql_connection.connect();
+
+//Check if some error occured to connection, reconect then.
+mysql_connection.on('error', function (error) {
+  console.log(error);
+  mysql_connection.connect();
+});
+
+// Connect to mysql databse and use database provided in BASIC configuration by default.
+mysql_connection.query(db_query.use_database, db_query.execute.log);
+
+// IIFE to create basic table structure (IF NOT EXISTS).
+(function initialize_tables() {
+  mysql_connection.query(db_query.create.user_table, db_query.execute.table_created);
+  mysql_connection.query(db_query.create.user_email_table, db_query.execute.table_created);
+  mysql_connection.query(db_query.create.user_datetime_table, db_query.execute.table_created);
+}());
 
 // Export created connection.
 module.exports = {connection: mysql_connection};
--- Database/query.js
+++ Database/query.js
@@ -22,25 +22,6 @@ const message = require('../Constants/index.js').MESSAGE;
 // Create empty object to put different query's.
 var query = {};
 
-// Create connection to mySQL database.
-mysql.connection.connect();
-
-//Check if some error occured to connection, reconect then.
-mysql.connection.on('error', function (error) {
-  console.log(error);
-  mysql.connection.connect();
-});
-
-// Connect to mysql databse and use database provided in BASIC configuration by default.
-mysql.connection.query(db_query.use_database, db_query.execute.log);
-
-// IIFE to create basic table structure (IF NOT EXISTS).
-(function initialize_tables() {
-  mysql.connection.query(db_query.create.user_table, db_query.execute.table_created);
-  mysql.connection.query(db_query.create.user_email_table, db_query.execute.table_created);
-  mysql.connection.query(db_query.create.user_datetime_table, db_query.execute.table_created);
-}());
-
 // Query to register new user.
 query.register_user = function (user) {
   mysql.connection.query(db_query.user.register_user, [user.user_name, user.first_name, user.last_name], function (error, rows, fields) {
@@ -52,32 +33,34 @@ query.register_user = function (user) {
 
     // Register other details after creating parent row.
     async.parallel([
-      function user_emial_register(callback) {
+      function user_email_register(callback) {
         mysql.connection.query(db_query.user.register_user_email, [user.user_name, user.email, user.password], function (error, rows, fields) {
-          if(error) return callback(error);
-          callback(null, rows);
+          if(error) return callback(db_errors.query.error.replace('%', 'Query'));
+          callback(null, true);
         })
       },
       function user_datetime_registration(callback) {
         mysql.connection.query(db_query.user.register_user_datetime, [user.user_name, new Date().toJSON(), new Date().toJSON()], function (error, rows, fields) {
-          if(error) return callback(error);
-          callback(null, rows);
+          if(error) return callback(db_errors.query.error.replace('%', 'Query'));
+          callback(null, true);
         })
+      },
+      function create_token(callback) {
+        JWT.sign({user: user.first_name, date: new Date()}, jwt_token.secret, jwt_token.options, function (error, token) {
+          if(error) callback(message.token.error);
+          else {
+            console.log(message.token.success);
+            callback(null, {message: db_errors.user.success, access_token: token});
+          }
+        });
       }
     ], function (error, result) {
       if (error) {
-        console.log(db_errors.query.error.replace('%', 'Query'));
+        console.log(error);
         return db_errors.user.error;
       }
-      JWT.sign({user: user.first_name, date: new Date()}, jwt_token.secret, jwt_token.options, function (error, token) {
-        if(error)
-          console.log(message.token.error);
-        else {
-          console.log(message.token.success);
-          console.log(db_errors.user.success);
-          return {message: db_errors.user.success, access_token: token};
-        }
-      });
+      console.log(db_errors.user.success);
+      return result[result.length - 1];
     });
   });
 };
